f2de6f2b3f374d90548bd40bb0ee5e0f6e7db0c3
gracehillen <grahillen19@ahastars.org>
2018-07-25 15:09:29 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -11,5 +11,5 @@ def change_rainbow_colors
 end
 
 def add_colors
-  # Write a solution that adds "green", then "blue" to the rainbow_colors array, then returns the array
+  return rainbow_colors 
 end
