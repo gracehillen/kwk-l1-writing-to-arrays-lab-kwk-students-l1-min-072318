8d7e401f099a5613d37e5522047a01b59ba68563
gracehillen <grahillen19@ahastars.org>
2018-07-25 15:17:48 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -5,16 +5,16 @@ rainbow_colors = [
 ]
 
 def change_rainbow_colors
-  return rainbow_colors[0] = "red"
-  return rainbow_colors[1] = "light_red"
+  rainbow_colors[0] = "red"
+  rainbow_colors[1] = "light_red"
   rainbow_colors[2] = "light_yellow"
 end
 
 change_rainbow_colors
 
 def add_colors
-  return rainbow_colors.push("green")
-  return rainbow_colors.push("blue")
+  rainbow_colors.push("green")
+  rainbow_colors.push("blue")
 end
 
 add_colors
